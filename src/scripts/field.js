fab16fa15af7645f5bb85c624bc065762564d1ef
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-08-21 14:44:55 -0500

    finished tilling

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,22 +1,34 @@
-let emptyField = []
+const emptyField = []
 
 export const addPlant = (seedObj) => {
-
-    if (seedObj.type = "asparagus") {
-        emptyField.push(seedObj)
-    } else if (seedObj.type = "corn") {
-        emptyField.push(seedObj[0]),
-        emptyField.push(seedObj[1])
-    } else if (seedObj.type = "potato") {
-        emptyField.push(seedObj)
-    } else if (seedObj.type = "soybean") {
-        emptyField.push(seedObj)
-    } else if (seedObj.type = "sunflower") {
-        emptyField.push(seedObj)
-    } else {
-        emptyField.push(seedObj)
-    } 
+    emptyField.push(seedObj)
+    // if (Array.isArray(seedObj) === false) {
+    //     console.log("It is an object, push it!")
+    //     emptyField.push(seedObj)
+    // } else {
+    //     for (const seed of seedObj) {
+    //         const cornSeed = seedObj[seed]
+    //         emptyField.push(cornSeed)
+    //     }
+    // }
+    //   for (const plant of seedObj) {
+         
+    //    }
+    // if (seedObj.type = "asparagus") {
+    //     emptyField.push(seedObj)
+    // } else if (seedObj.isArray = "corn") {
+    //     emptyField.push(seedObj[0]),
+    //     emptyField.push(seedObj[1])
+    // } else if (seedObj.type = "potato") {
+    //     emptyField.push(seedObj)
+    // } else if (seedObj.type = "soybean") {
+    //     emptyField.push(seedObj)
+    // } else if (seedObj.type = "sunflower") {
+    //     emptyField.push(seedObj)
+    // } else {
+    //     emptyField.push(seedObj)
+    // } 
         
 }
 
-export const usePlants 
+export const usePlants = () => { return emptyField }
