172452087a61ea8659ab0c30c6d2143352562807
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-08-20 19:22:13 -0500

    resolved issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 console.log("Welcome to the main module")
 
+import { createPlan } from './plan.js';
+
+const yearlyPlan = createPlan()
+
+console.log(yearlyPlan)
+
 import { createAsparagus } from "./seeds/asparagus.js"
 
 const asparagusSeed = createAsparagus()
